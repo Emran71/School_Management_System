ca4d3ea0de5f5d95a1b7286ec9035eb00d7ace39
emran71 <ekhan4475@gmail.com>
2025-04-23 15:08:16 +0600

    updated on 22/04/2025

---
+++ script.js
@@ -0,0 +1,40 @@
+function showSection(idname){
+  const allObject = document.querySelectorAll(".main-content > div");
+  allObject.forEach((object) => {
+    object.classList.add("hidden");
+  })
+  const object = document.getElementById(idname +"Section");
+  object.style.transition = "all 0.5s ease";
+  object.classList.remove("hidden");
+
+};
+
+
+
+function loginPage(){
+  window.location.href = "login.html";
+}
+function homepage(){
+  window.location.href = "index.html";
+}
+
+function submitForm() {
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+
+    if (username === 'admin' && password === 'password123') {
+        window.location.href = 'index.html';
+    } else {
+        document.getElementById('errorMessage').style.display = 'block';
+    }
+  });
+}
+
+
+
+
+
